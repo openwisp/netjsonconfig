1fa5e13afbda44a033505e2e17e9effc0a1c7ac0
Federico Capoano <nemesis@ninux.org>
2015-09-15 12:15:50 +0200

    Added test for multiple addresses

--- tests/test_openwrt.py
+++ tests/test_openwrt.py
@@ -7,7 +7,7 @@ from netjsonconfig import OpenWrt
 class TestOpenWrt(unittest.TestCase):
     """ OpenWrt tests """
 
-    def test_gen_loopback(self):
+    def test_loopback(self):
         o = OpenWrt({
             "type": "DeviceConfiguration",
             "interfaces": [
@@ -30,5 +30,42 @@ class TestOpenWrt(unittest.TestCase):
 config interface 'lo'
     option ifname 'lo'
     list ipaddr '127.0.0.1/8'
+"""
+        self.assertEqual(o.gen(), expected)
+
+    def test_multiple_ip(self):
+        o = OpenWrt({
+            "type": "DeviceConfiguration",
+            "interfaces": [
+                {
+                    "name": "eth0",
+                    "type": "ethernet",
+                    "addresses": [
+                        {
+                            "address": "192.168.1.1",
+                            "mask": 24,
+                            "family": "ipv4"
+                        },
+                        {
+                            "address": "192.168.2.1",
+                            "mask": 24,
+                            "family": "ipv4"
+                        },
+                        {
+                            "address": "fd87::1",
+                            "mask": 128,
+                            "family": "ipv6"
+                        }
+                    ]
+                }
+            ]
+        })
+        expected = """package network
+
+config interface 'eth0'
+    option ifname 'eth0'
+    list ipaddr '192.168.1.1/24'
+    list ipaddr '192.168.2.1/24'
+    list ip6addr 'fd87::1/128'
 """
         self.assertEqual(o.gen(), expected)
