76d28679b3357be8ce3797542fd54a16b81d073a
Federico Capoano <nemesis@ninux.org>
2015-09-15 18:19:19 +0200

    Added test for json method

--- tests/test_openwrt.py
+++ tests/test_openwrt.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 
 from netjsonconfig import OpenWrt
@@ -5,6 +6,27 @@ from netjsonconfig import OpenWrt
 
 class TestOpenWrt(unittest.TestCase):
     """ OpenWrt tests """
+
+    def test_json(self):
+        config = {
+            "type": "DeviceConfiguration",
+            "interfaces": [
+                {
+                    "name": "lo",
+                    "type": "loopback",
+                    "addresses": [
+                        {
+                            "address": "127.0.0.1",
+                            "mask": 8,
+                            "proto": "static",
+                            "family": "ipv4"
+                        }
+                    ]
+                }
+            ]
+        }
+        o = OpenWrt(config)
+        self.assertEqual(json.loads(o.json(indent=4)), config)
 
     def test_loopback(self):
         o = OpenWrt({
@@ -31,6 +53,7 @@ config interface 'lo'
     option proto 'static'
     option ipaddr '127.0.0.1/8'
 """
+        print(o.render())
         self.assertEqual(o.render(), expected)
 
     def test_multiple_ip(self):
@@ -80,6 +103,8 @@ config interface 'eth0_1_3'
     option proto 'static'
     option ip6addr 'fd87::1/128'
 """
+        print(o.render())
+        print(expected)
         self.assertEqual(o.render(), expected)
 
     def test_dhcp(self):
