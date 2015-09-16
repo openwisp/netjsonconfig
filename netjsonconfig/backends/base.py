14aad17399f20d229a56d91611f413dea8a5b61d
Federico Capoano <nemesis@ninux.org>
2015-09-16 16:36:09 +0200

    Ensure output always ends with 1 new line

--- netjsonconfig/backends/base.py
+++ netjsonconfig/backends/base.py
@@ -12,9 +12,16 @@ class BaseRenderer(object):
         """
         Performs cleanup of output (indentation, new lines)
         """
-        return output.replace('    ', '')\
-                     .replace('option', '    option')\
-                     .replace('list', '    list')[0:-1]
+        output = output.replace('    ', '')\
+                       .replace('option', '    option')\
+                       .replace('list', '    list')
+        # if output is present
+        # ensure it always ends with 1 new line
+        if output and not output.endswith('\n'):
+            output += '\n'
+        if output.endswith('\n\n'):
+            return output[0:-1]
+        return output
 
     def render(self):
         """
