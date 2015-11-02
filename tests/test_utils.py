d71a72ccf7cd48a35a245bfaebb7a8deefc8ba56
Federico Capoano <nemesis@ninux.org>
2015-11-02 17:17:57 +0100

    Refactored test_utils #26

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,21 +8,22 @@ class TestUtils(unittest.TestCase):
     tests for netjsonconfig.utils
     """
     def test_merge_config(self):
+        template = {"a": "a", "c": "template"}
+        config = {"b": "b", "c": "config"}
+        result = merge_config(template, config)
+        self.assertEqual(result, {
+            "a": "a",
+            "b": "b",
+            "c": "config"
+        })
+
+    def test_merge_config_dict(self):
         template = {
-            "dict": {
-                "a": "a"
-            },
-            "list": [
-                "element1"
-            ]
+            "dict": {"a": "a"},
         }
         config = {
-            "dict": {
-                "b": "b"
-            },
-            "list": [
-                "element2"
-            ]
+            "dict": {"b": "b"},
+            "key": "val"
         }
         result = merge_config(template, config)
         self.assertEqual(result, {
@@ -30,6 +31,18 @@ class TestUtils(unittest.TestCase):
                 "a": "a",
                 "b": "b"
             },
+            "key": "val"
+        })
+
+    def test_merge_config_list(self):
+        template = {
+            "list": ["element1"]
+        }
+        config = {
+            "list": ["element2"]
+        }
+        result = merge_config(template, config)
+        self.assertEqual(result, {
             "list": [
                 "element1",
                 "element2"
