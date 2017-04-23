d2e62ffdad9679431b3644218ee1d17022e5222d
Edoardo Putti <edoardo.putti@gmail.com>
2017-04-23 02:25:44 +0200

    [airos] draft for section resolv in SystemRenderer

--- netjsonconfig/backends/airos/renderers.py
+++ netjsonconfig/backends/airos/renderers.py
@@ -5,3 +5,15 @@ def BaseAirOSRenderer(BaseRenderer):
 
     def cleanup(self, output):
        return output 
+
+
+class SystemRenderer(BaseAirOSRenderer):
+    """
+    Write configuration for
+    - resolv
+    - system
+    - users
+    """
+
+    def _get_resolv(self):
+        dns_server = self.config.get('dns_server', [])
