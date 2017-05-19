24eecf428589837781b3f4b5c5c320756d21d0ff
Ritwick DSouza <ritwick.dsouza@outlook.com>
2017-05-19 18:19:12 +0400

    [raspbian] Added rasbian support to __init__

--- netjsonconfig/__init__.py
+++ netjsonconfig/__init__.py
@@ -3,3 +3,4 @@ from .version import VERSION, __version__, get_version  # noqa
 from .backends.openwrt.openwrt import OpenWrt  # noqa
 from .backends.openwisp.openwisp import OpenWisp  # noqa
 from .backends.openvpn.openvpn import OpenVpn  # noqa
+from .backedns.raspbian.raspbian import Raspbian #noqa
