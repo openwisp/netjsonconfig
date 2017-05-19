871fbac5c6a39ff57b435593f370c621946554ce
Ritwick DSouza <ritwick.dsouza@outlook.com>
2017-05-19 18:25:57 +0400

    [raspbian] Modified schema.py

--- netjsonconfig/backends/raspbian/schema.py
+++ netjsonconfig/backends/raspbian/schema.py
@@ -2,6 +2,4 @@ from ..schema import schema as default_schema
 from ..schema import DEFAULT_FILE_MODE
 from ..utils import merge_config
 
-schema = merge_config(default_schema, {
-    #
-})
+schema = merge_config(default_schema, {})
