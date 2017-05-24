daa52054dcecd18138df8ffe84d61ca5b6d99f21
Edoardo Putti <edoardo.putti@gmail.com>
2017-05-24 11:28:15 +0200

    [airos] added template and context for vlans

--- netjsonconfig/backends/airos/renderers.py
+++ netjsonconfig/backends/airos/renderers.py
@@ -37,6 +37,7 @@ class NetworkRenderer(BaseAirOSRenderer):
     """
     Write configuration for
     - bridge
+    - vlan
     """
 
     def _get_bridge(self):
@@ -45,3 +46,20 @@ class NetworkRenderer(BaseAirOSRenderer):
                 i for i in interfaces if i['type'] == 'bridge'
                 ]
         return reversed(list(enumerate(bridge_def)))
+
+    def _get_vlan(self):
+        interfaces = self.config.get('interfaces', [])
+        vlan_def = [
+                i for i in interfaces if '.' in i['name']
+                ]
+
+        airos_vlan = []
+
+        for v in vlan_def:
+            airos_v = v.copy()
+            airos_v['devname'] = v['name'].split('.')[0]
+            airos_v['id'] = v['name'].split('.')[1]
+            airos_v['status'] = "enabled" if not v['disabled'] else "disabled"
+            airos_vlan.append(airos_v)
+
+        return enumerate(airos_vlan)
