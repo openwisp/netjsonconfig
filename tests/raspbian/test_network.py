5894fc110112436f568238cd77bfc5483cdcdab5
Ritwick DSouza <ritwick.dsouza@outlook.com>
2017-05-26 02:20:35 +0400

    [raspbian] Added test for dns_server

--- tests/raspbian/test_network.py
+++ tests/raspbian/test_network.py
@@ -1,3 +1,16 @@
-import unitest
+import unittest
 
 from netjsonconfig import Raspbian
+from netjsonconfig.exceptions import ValidationError
+from netjsonconfig.utils import _TabsMixin
+
+class TestNetworkRenderer(unittest.TestCase, _TabsMixin):
+    def test_dns_server(self):
+        o = Raspbian({
+            "dns_servers": [
+                "10.254.0.1",
+                "10.254.0.2"
+            ]
+        })
+        expected = self._tabs("""nameserver​ 10.254.0.1
+nameserver​ 10.254.0.2""")
