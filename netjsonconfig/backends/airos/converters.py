38b2376b8feebd72b08e299d20f444cdd113330e
Edoardo Putti <edoardo.putti@gmail.com>
2017-06-01 12:36:35 +0200

    [airos] updated converters to use the tree based intermediate configuration

--- netjsonconfig/backends/airos/converters.py
+++ netjsonconfig/backends/airos/converters.py
@@ -6,33 +6,29 @@ class Bridge(BaseConverter):
 
     def to_intermediate(self):
         result = []
-        original = list(enumerate([
+        original = [
                 i for i in get_copy(self.netjson, self.netjson_key) if i['type'] == 'bridge'
-                ]))
+                ]
 
-        for index, interface in original:
-            result.append({
-                'key' : '{n}.comment'.format(n= index + 1 ),
-                'value' : interface.get('comment', ''),
-            })
-            result.append({
-                'key' : '{n}.devname'.format(n= index + 1 ),
-                'value' : interface['name'],
-            })
-
-            for port_index, port in enumerate(interface.get('bridge_members',[])):
-                result.append({
-                    'key': '{n}.port.{m}.devname'.format(n= index + 1, m= port_index + 1),
-                    'value': port,
-                })
-                result.append({
-                    'key': '{n}.port.{m}.status'.format(n= index + 1, m= port_index + 1),
-                    'value': 'enabled',
+        bridges = []
+        for interface in original:
+            bridge_ports = []
+            for port in interface.get('bridge_members',[]):
+                bridge_ports.append({
+                    'devname' : port,
+                    'status' : 'enabled',
                 })
 
+            bridges.append({
+                'comment' : interface.get('comment', ''),
+                'devname' : interface['name'],
+                'port' : bridge_ports,
+            })
+
+
+        result.append(bridges)
         result.append({
-            'key': 'status',
-            'value': 'enabled',
+            'status': 'enabled',
         })
 
         return (('bridge', result),)
@@ -45,12 +41,14 @@ class Gui(BaseConverter):
     def to_intermediate(self):
         result = [
                 {
-                    'key': 'language',
-                    'value': 'it_IT',
+                    'language': 'it_IT',
                 },
                 {
-                    'key': 'network.advanced.status',
-                    'value': 'enabled',
+                    'network' : {
+                        'advanced' : {
+                            'status' : 'enabled',
+                        }
+                    }
                 }
         ]
         return (('gui', result),)
@@ -62,24 +60,17 @@ class Httpd(BaseConverter):
     def to_intermediate(self):
         result = [
                 {
-                    'key': 'https.port',
-                    'value': 443,
+                    'https' : {
+                        'port' : 443,
+                        'status' : 'enabled',
+                    },
                 },
                 {
-                    'key': 'https.status',
-                    'value': 'enabled',
-                },
-                {
-                    'key': 'port',
-                    'value': 80,
-                },
-                {
-                    'key': 'session.timeout',
-                    'value': 900,
-                },
-                {
-                    'key': 'status',
-                    'value': 'enabled',
+                    'port' : 80,
+                    'session' : {
+                        'timeout' : 900,
+                    },
+                    'status': 'enabled',
                 },
         ]
 
@@ -93,24 +84,26 @@ class Resolv(BaseConverter):
         result = []
 
         result.append({
-            'key': 'host.1.name',
-            'value': '',
+            'host': [{
+                'name' : ''
+            }]
         })
 
-        original = list(enumerate(get_copy(self.netjson, self.netjson_key)))
-        for index, nameserver in reversed(original):
-            result.append({
-                'key' : 'nameserver.{index}.ip'.format(index= index + 1 ),
-                'value' : nameserver,
-            })
-            result.append({
-                'key': 'nameserver.{index}.status'.format(index= index + 1),
-                'value': 'enabled',
+        original = get_copy(self.netjson, self.netjson_key)
+
+        a = {
+                'nameservers' : [],
+        }
+        for nameserver in original:
+            a['nameservers'].append({
+                'ip': nameserver,
+                'status': 'enabled',
             })
 
+        result.append(a)
+
         result.append({
-            'key': 'status',
-            'value': 'enabled',
+            'status': 'enabled',
         })
 
         return (('resolv', result),)
@@ -122,20 +115,10 @@ class Snmp(BaseConverter):
     def to_intermediate(self):
         result = [
                 {
-                    'key': 'community',
-                    'value': 'public',
-                },
-                {
-                    'key': 'contact',
-                    'value': '',
-                },
-                {
-                    'key': 'location',
-                    'value': '',
-                },
-                {
-                    'key': 'status',
-                    'value': 'enabled',
+                    'community' : 'public',
+                    'contact' : 'value',
+                    'location': '',
+                    'status': 'enabled',
                 },
         ]
 
@@ -148,31 +131,22 @@ class Vlan(BaseConverter):
 
     def to_intermediate(self):
         result = []
-        original = list(enumerate([
+        original = [
                 i for i in get_copy(self.netjson, self.netjson_key) if '.' in i['name']
-                ]))
+                ]
 
-        for index, v in original:
-            result.append({
-                'key' : '{index}.comment'.format(index= index + 1 ),
-                'value' : v.get('comment', ''),
-            })
-            result.append({
-                'key' : '{index}.devname'.format(index= index + 1 ),
-                'value' : v['name'].split('.')[0],
-            })
-            result.append({
-                'key' : '{index}.id'.format(index= index + 1 ),
-                'value' : v['name'].split('.')[1],
-            })
-            result.append({
-                'key' : '{index}.status'.format(index= index + 1 ),
-                'value' : 'disabled' if v['disabled'] else 'enabled',
+        vlans = []
+        for v in original:
+            vlans.append({
+                'comment' : v.get('comment', ''),
+                'devname' : v['name'].split('.')[0],
+                'id' : v['name'].split('.')[1],
+                'status' : 'disabled' if v['disabled'] else 'enabled',
             })
 
+        result.append(vlans)
         result.append({
-            'key': 'status',
-            'value': 'enabled',
+            'status': 'enabled',
         })
 
         return (('vlan', result),)
@@ -183,49 +157,27 @@ class Wireless(BaseConverter):
 
     def to_intermediate(self):
         result = []
-        original = list(enumerate([
+        original = [
                 i for i in get_copy(self.netjson, self.netjson_key) if hasattr(i,'wireless')
-                ]))
+                ]
 
-        for index, w in original:
-            result.append({
-                'key' : '{index}.addmtikie'.format(index= index + 1 ),
-                'value' : 'enabled',
-            })
-            result.append({
-                'key' : '{index}.devname'.format(index= index + 1 ),
-                'value' : w['name'],
-            })
-
+        ws = []
+        for w in original:
             hide_ssid = 'enabled' if w['wireless']['hide_ssid'] else 'disabled'
-            result.append({
-                'key' : '{index}.hide_ssid'.format(index= index + 1 ),
-                'value' : hide_ssid
-            })
-
             encryption = w['wireless'].get('encryption', 'none')
-            result.append({
-                'key' : '{index}.security.type'.format(index= index + 1 ),
-                'value' : encryption,
+            ws.append({
+                'addmtikie': 'enabled',
+                'devname' : w['name'],
+                'hide_ssid' : hide_ssid,
+                'security' : { 'type' : encryption },
+                'ssid' : w['wireless']['ssid'],
+                'status' : 'enabled',
+                'wds' : { 'status': 'enabled' },
             })
+        result.append(ws)
 
-            result.append({
-                'key' : '{index}.ssid'.format(index= index + 1 ),
-                'value' : w['wireless']['ssid'],
-            })
-
-            result.append({
-                'key' : '{index}.status'.format(index= index + 1 ),
-                'value' : 'enabled',
-            })
-
-            result.append({
-                'key' : '{index}.wds.status'.format(index= index + 1 ),
-                'value' : 'enabled',
-            })
         result.append({
-            'key': 'status',
-            'value': 'enabled',
+            'status': 'enabled',
         })
 
         return (('wireless', result),)
