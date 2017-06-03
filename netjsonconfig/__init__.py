f22c0dee1863b24b940007dc85158dada3d67279
Ritwick DSouza <ritwick.dsouza@outlook.com>
2017-06-04 04:19:16 +0530

    [raspbian] Fixed flake8 and isort warnings

--- netjsonconfig/__init__.py
+++ netjsonconfig/__init__.py
@@ -3,4 +3,4 @@ from .version import VERSION, __version__, get_version  # noqa
 from .backends.openwrt.openwrt import OpenWrt  # noqa
 from .backends.openwisp.openwisp import OpenWisp  # noqa
 from .backends.openvpn.openvpn import OpenVpn  # noqa
-from .backends.raspbian.raspbian import Raspbian #noqa
+from .backends.raspbian.raspbian import Raspbian  # noqa
