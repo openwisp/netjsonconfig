353a026ff0f0f324e42b489c16c007e51c4de8b4
Edoardo Putti <edoardo.putti@gmail.com>
2017-06-12 13:53:55 +0200

    [test] added test for inactive radio

--- tests/airos/test_wireless.py
+++ tests/airos/test_wireless.py
@@ -60,3 +60,56 @@ class TestWirelessConverter(unittest.TestCase):
                 ]
 
         self.assertEqual(o.intermediate_data['wireless'], expected)
+
+    def test_inactive_wireless(self):
+
+        o = self.backend({
+            "interfaces": [
+                {
+                    "type": "wireless",
+                    "name": "wlan0",
+                    "mac": "de:9f:db:30:c9:c5",
+                    "mtu": 1500,
+                    "txqueuelen": 1000,
+                    "autostart": True,
+                    "disabled": True,
+                    "wireless": {
+                        "radio": "radio0",
+                        "mode": "access_point",
+                        "ssid": "ap-ssid-example",
+                    },
+                    "addresses": [
+                        {
+                            "address": "192.168.1.1",
+                            "mask": 24,
+                            "family": "ipv4",
+                            "proto": "static",
+                        }
+                    ],
+                }
+            ]
+        })
+
+        o.to_intermediate()
+
+        expected = [
+                    {
+                        '1.addmtikie': 'enabled',
+                        '1.devname': 'wlan0',
+                        '1.hide_ssid': 'disabled',
+                        '1.security.type': 'none',
+                        '1.signal_led1': 75,
+                        '1.signal_led2': 50,
+                        '1.signal_led3': 25,
+                        '1.signal_led4': 15,
+                        '1.signal_led_status': 'enabled',
+                        '1.ssid': 'ap-ssid-example',
+                        '1.status': 'disabled',
+                        '1.wds.status': 'enabled',
+                    },
+                    {
+                        'status': 'enabled',
+                    }
+                ]
+
+        self.assertEqual(o.intermediate_data['wireless'], expected)
