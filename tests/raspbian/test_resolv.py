6748f9e0f5ef37dba66fe21befed226bb7abb157
Ritwick DSouza <ritwick.dsouza@outlook.com>
2017-06-13 00:36:49 +0530

    [raspbian] Added tests for resolv and minor modifications in interfaces converter

--- tests/raspbian/test_resolv.py
+++ tests/raspbian/test_resolv.py
@@ -1,8 +1,11 @@
 import unittest
 
 from netjsonconfig import Raspbian
+from netjsonconfig.exceptions import ValidationError
+from netjsonconfig.utils import _TabsMixin
 
-class TestResovlConverter(unittest.TestCase):
+
+class TestResovlRenderer(unittest.TestCase, _TabsMixin):
 
     def test_dns_server(self):
         o = Raspbian({
@@ -12,16 +15,16 @@ class TestResovlConverter(unittest.TestCase):
             ],
         })
 
-        o.to_intermediate()
+        expected = '''/etc/network/interfaces
+-----------------------
 
-        expected = [OrderedDict([
-            ('nameserver', [
-                {'ip': '10.254.0.1'},
-                {'ip': '10.254.0.2'}
-                ])
-            ])
-        ]
-        self.assertEqual(o.intermediate_data['dns_servers'], expected)
+
+/etc/resolv.conf
+----------------
+nameserver 10.254.0.1
+nameserver 10.254.0.2
+'''
+        self.assertEqual(o.render(), expected)
 
     def test_dns_search(self):
         o = Raspbian({
@@ -30,12 +33,49 @@ class TestResovlConverter(unittest.TestCase):
             ],
         })
 
-        o.to_intermediate()
+        expected = '''/etc/network/interfaces
+-----------------------
 
-        expected = [OrderedDict([
-            ('domain', [
-                {'domain': 'domain.com'}
-                ])
-            ])
-        ]
-        self.assertEqual(o.intermediate_data['dns_search'], expected)
+
+/etc/resolv.conf
+----------------
+search domain.com
+'''
+        self.assertEqual(o.render(), expected)
+
+    def test_dns_server_and_dns_search(self):
+        o = Raspbian({
+            "dns_servers": [
+                "10.11.12.13",
+                "8.8.8.8"],
+            "dns_search": [
+                "netjson.org",
+                "openwisp.org"
+            ],
+        })
+
+        expected = '''/etc/network/interfaces
+-----------------------
+
+
+/etc/resolv.conf
+----------------
+nameserver 10.11.12.13
+nameserver 8.8.8.8
+search netjson.org
+search openwisp.org
+'''
+        self.assertEqual(o.render(), expected)
+
+    def test_no_dns_server_and_dns_search(self):
+        o = Raspbian({
+        })
+
+        expected = '''/etc/network/interfaces
+-----------------------
+
+
+/etc/resolv.conf
+----------------
+'''
+        self.assertEqual(o.render(), expected)
