557d8f360002b6ec41a911f6f9138e4fda44bf25
Edoardo Putti <edoardo.putti@gmail.com>
2017-06-13 09:42:49 +0200

    [airos] remove empty settings from flattened structure

--- netjsonconfig/backends/airos/airos.py
+++ netjsonconfig/backends/airos/airos.py
@@ -52,7 +52,7 @@ class AirOS(BaseBackend):
     def to_intermediate(self):
         super(AirOS, self).to_intermediate()
         for k,v in self.intermediate_data.items():
-            self.intermediate_data[k] = flatten(intermediate_to_list(v))
+            self.intermediate_data[k] = filter(lambda x: x != {}, flatten(intermediate_to_list(v)))
 
 def flatten(xs):
     """
