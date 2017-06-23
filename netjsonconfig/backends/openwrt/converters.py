openwisp/netjsonconfig

commit 4f8d105108bbd7ede4ae12360fb7380e4b0dafbb

[openwrt] Added support for "ip6gw" option #86
- updated tests
- updated docs

Closes #86

netjsonconfig/backends/openwrt/converters.py
@@ -126,6 +126,9 @@ class Interfaces(BaseConverter):
                 address['proto'] = 'dhcp' if family == 'ipv4' else 'dhcpv6'
                 dhcp.append(self.__del_address_keys(address))
                 continue
+            if 'gateway' in address:
+                uci_key = 'gateway' if family == 'ipv4' else 'ip6gw'
+                interface[uci_key] = address['gateway']
             # static
             address_key = 'ipaddr' if family == 'ipv4' else 'ip6addr'
             static.setdefault(address_key, [])
@@ -178,7 +181,7 @@ class Interfaces(BaseConverter):
             del interface['addresses']
         return interface
 
-    _address_keys = ['address', 'mask', 'family']
+    _address_keys = ['address', 'mask', 'family', 'gateway']
 
     def __del_address_keys(self, address):
         """

tests/openwrt/test_network.py
@@ -44,26 +44,28 @@ config interface 'lo'
                     "autostart": True,
                     "addresses": [
                         {
-                            "address": "192.168.1.1",
+                            "address": "192.168.1.2",
+                            "gateway": "192.168.1.1",
                             "mask": 24,
                             "proto": "static",
                             "family": "ipv4"
                         },
                         {
-                            "address": "192.168.2.1",
+                            "address": "192.168.2.3",
                             "mask": 24,
                             "proto": "static",
                             "family": "ipv4"
                         },
                         {
-                            "address": "fd87::1",
-                            "mask": 128,
+                            "address": "fd87::2",
+                            "gateway": "fd87::1",
+                            "mask": 64,
                             "proto": "static",
                             "family": "ipv6"
                         },
                         {
-                            "address": "fd87::2",
-                            "mask": 128,
+                            "address": "fd87::3",
+                            "mask": 64,
                             "proto": "static",
                             "family": "ipv6"
                         }
@@ -75,11 +77,13 @@ config interface 'lo'
 
 config interface 'eth0_1'
     option auto '1'
+    option gateway '192.168.1.1'
     option ifname 'eth0.1'
-    list ip6addr 'fd87::1/128'
-    list ip6addr 'fd87::2/128'
-    list ipaddr '192.168.1.1/24'
-    list ipaddr '192.168.2.1/24'
+    list ip6addr 'fd87::2/64'
+    list ip6addr 'fd87::3/64'
+    option ip6gw 'fd87::1'
+    list ipaddr '192.168.1.2/24'
+    list ipaddr '192.168.2.3/24'
     option proto 'static'
 """)
         self.assertEqual(o.render(), expected)

commit 5f8483e648c33647166b45bb3a11c028e16492fe

[openwrt] Fixed repeated bridge gateway case
Bug inadvertently introduced in 4f8d105

netjsonconfig/backends/openwrt/converters.py
@@ -215,8 +215,9 @@ class Interfaces(BaseConverter):
             # to these physical names
             interface['ifname'] = 'br-{ifname}'.format(**interface)
             # do not repeat bridge attributes (they have already been processed)
-            del interface['type']
-            del interface['bridge_members']
+            for attr in ['type', 'bridge_members', 'stp', 'gateway']:
+                if attr in interface:
+                    del interface[attr]
         elif interface['type'] != 'bridge':
             del interface['type']
         return interface