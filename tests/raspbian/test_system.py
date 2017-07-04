8bea79a6c278af97a57d4d4b94b6e0d8f16691c7
Ritwick DSouza <ritwick.dsouza@outlook.com>
2017-07-04 16:55:00 +0530

    [raspbian] Added support for NTP

---
+++ tests/raspbian/test_system.py
@@ -0,0 +1,27 @@
+import unittest
+
+from netjsonconfig import Raspbian
+from netjsonconfig.utils import _TabsMixin
+
+class TestSystemRender(unittest.TestCase, _TabsMixin):
+
+    def test_ntp(self):
+        o = Raspbian({
+            "ntp": {
+                "enabled": True,
+                "enable_server": False,
+                "server": [
+                    "0.openwrt.pool.ntp.org",
+                    "1.openwrt.pool.ntp.org",
+                    "2.openwrt.pool.ntp.org",
+                    "3.openwrt.pool.ntp.org"
+                ]
+            }
+        })
+
+        expected = '''config: /etc/ntp.config
+server 0.openwrt.pool.ntp.org
+server 1.openwrt.pool.ntp.org
+server 2.openwrt.pool.ntp.org
+server 3.openwrt.pool.ntp.org
+'''
