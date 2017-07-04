cb3a28955a3e6211cce187fec3ce19a08767aec2
Ritwick DSouza <ritwick.dsouza@outlook.com>
2017-07-04 17:00:54 +0530

    [raspbian] Fix isort and flake8 errors

--- netjsonconfig/backends/raspbian/converters.py
+++ netjsonconfig/backends/raspbian/converters.py
@@ -154,9 +154,9 @@ class Ntp(BaseConverter):
     netjson_key = 'ntp'
 
     def to_intermediate(self):
-        result  = []
+        result = []
         ntp = get_copy(self.netjson, self.netjson_key)
-        if ntp.get('enabled', False) == True:
+        if ntp.get('enabled', False):
             for server in ntp.get('server'):
                 result.append(server)
         return (('ntp', result),)
