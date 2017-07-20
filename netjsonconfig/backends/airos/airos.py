79c1c755adf08273389c5e3f70d487d1dedaf6ed
Edoardo Putti <edoardo.putti@gmail.com>
2017-07-20 12:54:24 +0200

    [airos] moved conversion functions to separate module

--- netjsonconfig/backends/airos/airos.py
+++ netjsonconfig/backends/airos/airos.py
@@ -1,12 +1,10 @@
-from six import string_types
-from six.moves import reduce
-
 from ..base.backend import BaseBackend
 from .converters import (Aaa, Bridge, Discovery, Dyndns, Ebtables, Gui, Httpd,
                          Igmpproxy, Iptables, Netconf, Netmode, Ntpclient,
                          Pwdog, Radio, Resolv, Route, Snmp, Sshd, Syslog,
                          System, Telnetd, Update, Users, Vlan, Wireless,
                          Wpasupplicant)
+from .intermediate import flatten, intermediate_to_list
 from .renderers import AirOsRenderer
 from .schema import schema
 
@@ -56,96 +54,3 @@ class AirOs(BaseBackend):
         super(AirOs, self).to_intermediate()
         for k, v in self.intermediate_data.items():
             self.intermediate_data[k] = [x for x in flatten(intermediate_to_list(v)) if x != {}]
-
-
-def flatten(elements):
-    """
-    Flatten a list
-    """
-    if elements is not list:
-        return elements
-    else:
-        return reduce(lambda x, y: x + flatten(y), elements, [])
-
-
-def intermediate_to_list(configuration):
-    """
-    Explore the configuration tree and flatten where
-    possible with the following policy
-    - list -> prepend the list index to every item key
-    - dictionary -> prepend the father key to every key
-
-    configuration :: List[Enum[Dict,List]]
-    return List[Dict]
-
-    >>> intermediate_to_list([
-        {
-            'spam': {
-                'eggs': 'spam and eggs'
-            }
-        }
-    ])
-    >>>
-    [{
-        'spam.eggs' : 'spam and eggs'
-    ]}
-
-    >>> intermediate_to_list([
-        {
-            'spam': {
-                'eggs': 'spam and eggs'
-            }
-        },
-        [
-            {
-                'henry': 'the first'
-            },
-            {
-                'jacob' : 'the second'
-            }
-        ]
-    ])
-    >>>
-    [
-        {
-            'spam.eggs' : 'spam and eggs'
-        },
-        {
-            '1.henry' : 'the first'
-        },
-        {
-            '2.jacob' : 'the second'
-        }
-    ]
-    """
-
-    result = []
-
-    for element in configuration:
-        temp = {}
-        if isinstance(element, list):
-            for index, el in enumerate(element):
-                for key, value in el.items():
-                    temp['{i}.{key}'.format(i=index + 1, key=key)] = value
-            result = result + intermediate_to_list([temp])
-
-        elif isinstance(element, dict):
-            for key, value in element.items():
-                if isinstance(value, string_types) or isinstance(value, int):
-                    temp[key] = value
-                else:
-                    # reduce to atom list
-                    # as value could be dict or list
-                    # enclose it in a flattened list
-                    for child in intermediate_to_list(flatten([value])):
-                        for child_key, child_value in child.items():
-                            nested_key = '{key}.{subkey}'.format(key=key, subkey=child_key)
-                            temp[nested_key] = child_value
-
-            # now it is atomic, append it to
-            result.append(temp)
-
-        else:
-            raise Exception('malformed intermediate representation')
-
-    return result
