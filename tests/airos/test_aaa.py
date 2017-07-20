bdd19ec894a1c2df746bcf2e33052439e890846c
Edoardo Putti <edoardo.putti@gmail.com>
2017-07-20 16:53:29 +0200

    [airos] added tests for wireless authentication in aaa section

--- tests/airos/test_aaa.py
+++ tests/airos/test_aaa.py
@@ -12,9 +12,142 @@ class TestResolvConverter(ConverterTest):
         })
         o.to_intermediate()
         expected = [
-                {
-                    'status': 'disabled',
-                },
+            {
+                'status': 'disabled',
+            },
         ]
+        self.assertEqualConfig(o.intermediate_data['aaa'], expected)
 
+    def test_ap_no_authentication(self):
+        o = self.backend({
+            "interfaces": [
+                {
+                    "type": "wireless",
+                    "name": "wlan0",
+                    "wireless": {
+                        "mode": "access_point",
+                        "radio": "ath0",
+                        "ssid": "i-like-pasta",
+                    },
+                },
+            ],
+        })
+        o.to_intermediate()
+        expected = [
+            {
+                'status': 'disabled',
+            },
+            {
+                '1.radius.acct.1.port': 1813,
+                '1.radius.acct.1.status': 'disabled',
+                '1.radius.auth.1.port': 1812,
+                '1.radius.auth.1.status': 'disabled',
+                '1.status': 'disabled',
+            },
+        ]
+        self.assertEqualConfig(o.intermediate_data['aaa'], expected)
+
+    def test_ap_psk_authentication(self):
+        o = self.backend({
+            "interfaces": [
+                {
+                    "type": "wireless",
+                    "name": "wlan0",
+                    "wireless": {
+                        "mode": "access_point",
+                        "radio": "ath0",
+                        "ssid": "i-like-pasta",
+                        "encryption": {
+                            "proto": "wpa2_personal",
+                            "key": "and-pizza-too",
+                        },
+                    },
+                },
+            ],
+        })
+        o.to_intermediate()
+        expected = [
+            {
+                'status': 'enabled',
+            },
+            {
+                '1.radius.acct.1.port': 1813,
+                '1.radius.acct.1.status': 'disabled',
+                '1.radius.auth.1.port': 1812,
+                '1.radius.auth.1.status': 'disabled',
+                '1.status': 'enabled',
+                # here begins magic
+                '1.radius.macacl.status': 'disabled', # move to radius method
+                '1.ssid': 'i-like-pasta',
+                '1.br.devname': 'br0', # only in bridge mode?
+                '1.devname': 'ath0',
+                '1.driver': 'madwifi',
+                '1.wpa.1.pairwise': 'CCMP',
+                '1.wpa.key.1.mgmt': 'WPA-PSK',
+                '1.wpa.mode': 2,
+                '1.wpa.psk': 'and-pizza-too',
+            }
+        ]
+        self.assertEqualConfig(o.intermediate_data['aaa'], expected)
+
+    def test_sta_no_authentication(self):
+        o = self.backend({
+            "interfaces": [
+                {
+                    "type": "wireless",
+                    "name": "wlan0",
+                    "wireless": {
+                        "mode": "station",
+                        "radio": "ath0",
+                        "ssid": "i-like-pasta",
+                    },
+                },
+            ],
+        })
+        o.to_intermediate()
+        expected = [
+            {
+                'status': 'disabled',
+            },
+            {
+                '1.radius.acct.1.port': 1813,
+                '1.radius.acct.1.status': 'disabled',
+                '1.radius.auth.1.port': 1812,
+                '1.radius.auth.1.status': 'disabled',
+                '1.status': 'disabled',
+            },
+        ]
+        self.assertEqualConfig(o.intermediate_data['aaa'], expected)
+
+    def test_sta_psk_authentication(self):
+        o = self.backend({
+            "interfaces": [
+                {
+                    "type": "wireless",
+                    "name": "wlan0",
+                    "wireless": {
+                        "mode": "station",
+                        "radio": "ath0",
+                        "ssid": "i-like-pasta",
+                        "encryption": {
+                            "proto": "wpa2_personal",
+                            "key": "and-pizza-too",
+                        },
+                    },
+                },
+            ],
+        })
+        o.to_intermediate()
+        expected = [
+            {
+                'status': 'disabled',
+            },
+            {
+                '1.radius.acct.1.port': 1813,
+                '1.radius.acct.1.status': 'disabled',
+                '1.radius.auth.1.port': 1812,
+                '1.radius.auth.1.status': 'disabled',
+                '1.status': 'disabled',
+            },
+        ]
         self.assertEqualConfig(o.intermediate_data['aaa'], expected)
