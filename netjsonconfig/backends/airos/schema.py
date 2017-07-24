6dd82ee4cd5458c0c94b2f7ae2dcd48d8e68820e
Federico Capoano <nemesis@ninux.org>
2017-07-24 18:09:42 +0200

    [airos] Reformatted schema indentation etc

--- netjsonconfig/backends/airos/schema.py
+++ netjsonconfig/backends/airos/schema.py
@@ -22,133 +22,130 @@ default_ntp_servers = [
 ]
 
 override_schema = {
-        "type": "object",
-        "addtionalProperties": True,
-        "definitions": {
-            "base_address": {
-                "properties": {
-                    "management": {
-                        "type": "boolean",
-                        "default": False,
-                    }
-                }
-            },
-            "encryption_wireless_property_ap": {
-                "properties": {
-                    "encryption": {
-                        "type": "object",
-                        "title": "Encryption",
-                        "required": [
-                            "protocol",
-                        ],
-                        "propertyOrder": 20,
-                        "oneOf": [
-                            {"$ref": "#/definitions/encryption_none"},
-                            {"$ref": "#/definitions/encryption_wpa_personal"},
-                            {"$ref": "#/definitions/encryption_wpa_enterprise_sta"},
-                        ],
-                    },
-                },
-            },
-            "encryption_wireless_property_sta": {
-                "properties": {
-                    "encryption": {
-                        "type": "object",
-                        "title": "Encryption",
-                        "required": [
-                            "protocol",
-                        ],
-                        "propertyOrder": 20,
-                        "oneOf": [
-                            {"$ref": "#/definitions/encryption_none"},
-                            {"$ref": "#/definitions/encryption_wpa_personal"},
-                            {"$ref": "#/definitions/encryption_wpa_enterprise_sta"},
-                        ],
-                    },
-                },
-            },
-            "interface_settings": {
-                "properties": {
-                    "authoneg": {
-                        "type": "boolean",
-                    },
-                    "flowcontrol": {
-                        "type": "boolean",
-                    }
+    "type": "object",
+    "addtionalProperties": True,
+    "definitions": {
+        "base_address": {
+            "properties": {
+                "management": {
+                    "type": "boolean",
+                    "default": False,
                 }
             }
         },
-        "properties": {
-            "netmode": {
-                "enum": [
-                    "bridge",
-                    "router",
-                ],
-                "default": "bridge",
-                "type": "string",
-            },
-            "user": {
-                "additionalProperties": True,
-                "properties": {
-                    "name": {
-                        "type": "string",
-                    },
-                    "salt": {
-                        "type": "string",
-                    },
+        "encryption_wireless_property_ap": {
+            "properties": {
+                "encryption": {
+                    "type": "object",
+                    "title": "Encryption",
+                    "required": [
+                        "protocol",
+                    ],
+                    "propertyOrder": 20,
+                    "oneOf": [
+                        {"$ref": "#/definitions/encryption_none"},
+                        {"$ref": "#/definitions/encryption_wpa_personal"},
+                        {"$ref": "#/definitions/encryption_wpa_enterprise_sta"},
+                    ],
                 },
-                "required": [
-                    "name",
-                    "password",
-                    "salt",
-                ],
-            },
-            "ntp": {
-                "type": "object",
-                "title": "NTP Settings",
-                "additionalProperties": True,
-                "propertyOrder": 8,
-                "properties": {
-                    "enabled": {
-                        "type": "boolean",
-                        "title": "enable NTP client",
-                        "default": True,
-                        "format": "checkbox",
-                        "propertyOrder": 1,
-                    },
-                    "enable_server": {
-                        "type": "boolean",
-                        "title": "enable NTP server",
-                        "default": False,
-                        "format": "checkbox",
-                        "propertyOrder": 2,
-                    },
-                    "server": {
-                        "title": "NTP Servers",
-                        "description": "NTP server candidates",
-                        "type": "array",
-                        "uniqueItems": True,
-                        "additionalItems": True,
-                        "propertyOrder": 3,
-                        "items": {
-                            "title": "NTP server",
-                            "type": "string",
-                            "format": "hostname"
-                        },
-                        "default": default_ntp_servers,
-                    }
-                }
             },
         },
-    }
+        "encryption_wireless_property_sta": {
+            "properties": {
+                "encryption": {
+                    "type": "object",
+                    "title": "Encryption",
+                    "required": [
+                        "protocol",
+                    ],
+                    "propertyOrder": 20,
+                    "oneOf": [
+                        {"$ref": "#/definitions/encryption_none"},
+                        {"$ref": "#/definitions/encryption_wpa_personal"},
+                        {"$ref": "#/definitions/encryption_wpa_enterprise_sta"},
+                    ],
+                },
+            },
+        },
+        "interface_settings": {
+            "properties": {
+                "authoneg": {
+                    "type": "boolean",
+                },
+                "flowcontrol": {
+                    "type": "boolean",
+                }
+            }
+        }
+    },
+    "properties": {
+        "netmode": {
+            "enum": [
+                "bridge",
+                "router",
+            ],
+            "default": "bridge",
+            "type": "string",
+        },
+        "user": {
+            "additionalProperties": True,
+            "properties": {
+                "name": {
+                    "type": "string",
+                },
+                "salt": {
+                    "type": "string",
+                },
+            },
+            "required": [
+                "name",
+                "password",
+                "salt",
+            ],
+        },
+        "ntp": {
+            "type": "object",
+            "title": "NTP Settings",
+            "additionalProperties": True,
+            "propertyOrder": 8,
+            "properties": {
+                "enabled": {
+                    "type": "boolean",
+                    "title": "enable NTP client",
+                    "default": True,
+                    "format": "checkbox",
+                    "propertyOrder": 1,
+                },
+                "enable_server": {
+                    "type": "boolean",
+                    "title": "enable NTP server",
+                    "default": False,
+                    "format": "checkbox",
+                    "propertyOrder": 2,
+                },
+                "server": {
+                    "title": "NTP Servers",
+                    "description": "NTP server candidates",
+                    "type": "array",
+                    "uniqueItems": True,
+                    "additionalItems": True,
+                    "propertyOrder": 3,
+                    "items": {
+                        "title": "NTP server",
+                        "type": "string",
+                        "format": "hostname"
+                    },
+                    "default": default_ntp_servers,
+                }
+            }
+        },
+    },
+}
+
+schema = merge_config(default_schema, override_schema)
 
 schema['definitions']['encryption_wireless_property_ap'] = \
     override_schema['definitions']['encryption_wireless_property_ap']
-schema = merge_config(
-        default_schema,
-        override_schema
-        )
 
 schema['definitions']['encryption_wireless_property_sta'] = \
     override_schema['definitions']['encryption_wireless_property_sta']
-__all__ = [schema]
