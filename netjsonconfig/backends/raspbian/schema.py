450da4b4a3b4e27af6131e3fd5e449cb9c69c507
Ritwick DSouza <ritwick.dsouza@outlook.com>
2017-07-26 03:28:55 +0530

    [raspbian] Updated schema

--- netjsonconfig/backends/raspbian/schema.py
+++ netjsonconfig/backends/raspbian/schema.py
@@ -1,92 +1,94 @@
+from ...channels import channels_2and5, channels_2ghz, channels_5ghz
+from ...countries import countries
 from ...schema import schema as default_schema
 from ...utils import merge_config
 from ..openwrt.timezones import timezones
 
-schema = merge_config(default_schema, {
-    "definitions": {
-        "radio_hwmode_11g": {
-            "properties": {
-                "hwmode": {
-                    "type": "string",
-                    "title": "hardware mode",
-                    "readOnly": True,
-                    "propertyOrder": 8,
-                    "default": "11g",
-                    "enum": ["11g"],
-                }
-            }
-        },
-        "radio_hwmode_11a": {
-            "properties": {
-                "hwmode": {
-                    "type": "string",
-                    "title": "hardware mode",
-                    "readOnly": True,
-                    "propertyOrder": 8,
-                    "default": "11a",
-                    "enum": ["11a"],
-                }
-            }
-        },
-        "radio_80211gn_settings": {
-            "allOf": [{"$ref": "#/definitions/radio_hwmode_11g"}]
-        },
-        "radio_80211an_settings": {
-            "allOf": [{"$ref": "#/definitions/radio_hwmode_11a"}]
-        },
-        "radio_80211ac_2ghz_settings": {
-            "allOf": [{"$ref": "#/definitions/radio_hwmode_11g"}]
-        },
-        "radio_80211ac_5ghz_settings": {
-            "allOf": [{"$ref": "#/definitions/radio_hwmode_11a"}]
-        },
-    },
-    "properties": {
-        "general": {
-            "properties": {
-                "timezone": {
-                    "enum": list(timezones.keys()),
-                    "default": "UTC",
-                }
-            }
-        },
-        "ntp": {
-            "type": "object",
-            "title": "NTP Settings",
-            "additionalProperties": True,
-            "propertyOrder": 8,
-            "properties": {
-                "enabled": {
-                    "type": "boolean",
-                    "title": "enable NTP client",
-                    "default": True,
-                    "format": "checkbox",
-                    "propertyOrder": 1,
-                },
-                "enable_server": {
-                    "type": "boolean",
-                    "title": "enable NTP server",
-                    "default": False,
-                    "format": "checkbox",
-                    "propertyOrder": 2,
-                },
-                "server": {
-                    "title": "NTP Servers",
-                    "description": "NTP server candidates",
-                    "type": "array",
-                    "uniqueItems": True,
-                    "additionalItems": True,
-                    "propertyOrder": 3,
-                    "items": {
-                        "title": "NTP server",
-                        "type": "string",
-                        "format": "hostname"
-                    },
-                }
-            }
-        }
-    }
-})
+# schema = merge_config(default_schema, {
+#     "definitions": {
+#         "radio_hwmode_11g": {
+#             "properties": {
+#                 "hwmode": {
+#                     "type": "string",
+#                     "title": "hardware mode",
+#                     "readOnly": True,
+#                     "propertyOrder": 8,
+#                     "default": "11g",
+#                     "enum": ["11g"],
+#                 }
+#             }
+#         },
+#         "radio_hwmode_11a": {
+#             "properties": {
+#                 "hwmode": {
+#                     "type": "string",
+#                     "title": "hardware mode",
+#                     "readOnly": True,
+#                     "propertyOrder": 8,
+#                     "default": "11a",
+#                     "enum": ["11a"],
+#                 }
+#             }
+#         },
+#         "radio_80211gn_settings": {
+#             "allOf": [{"$ref": "#/definitions/radio_hwmode_11g"}]
+#         },
+#         "radio_80211an_settings": {
+#             "allOf": [{"$ref": "#/definitions/radio_hwmode_11a"}]
+#         },
+#         "radio_80211ac_2ghz_settings": {
+#             "allOf": [{"$ref": "#/definitions/radio_hwmode_11g"}]
+#         },
+#         "radio_80211ac_5ghz_settings": {
+#             "allOf": [{"$ref": "#/definitions/radio_hwmode_11a"}]
+#         },
+#     },
+#     "properties": {
+#         "general": {
+#             "properties": {
+#                 "timezone": {
+#                     "enum": list(timezones.keys()),
+#                     "default": "UTC",
+#                 }
+#             }
+#         },
+#         "ntp": {
+#             "type": "object",
+#             "title": "NTP Settings",
+#             "additionalProperties": True,
+#             "propertyOrder": 8,
+#             "properties": {
+#                 "enabled": {
+#                     "type": "boolean",
+#                     "title": "enable NTP client",
+#                     "default": True,
+#                     "format": "checkbox",
+#                     "propertyOrder": 1,
+#                 },
+#                 "enable_server": {
+#                     "type": "boolean",
+#                     "title": "enable NTP server",
+#                     "default": False,
+#                     "format": "checkbox",
+#                     "propertyOrder": 2,
+#                 },
+#                 "server": {
+#                     "title": "NTP Servers",
+#                     "description": "NTP server candidates",
+#                     "type": "array",
+#                     "uniqueItems": True,
+#                     "additionalItems": True,
+#                     "propertyOrder": 3,
+#                     "items": {
+#                         "title": "NTP server",
+#                         "type": "string",
+#                         "format": "hostname"
+#                     },
+#                 }
+#             }
+#         }
+#     }
+# })
 
 schema = {
     "$schema": "http://json-schema.org/draft-04/schema#",
@@ -786,6 +788,8 @@ schema = {
                     "propertyOrder": 1,
                 },
                 "timezone": {
+                    "type": "string",
+                    "propertyOrder": 1,
                     "enum": list(timezones.keys()),
                     "default": "UTC",
                 }
