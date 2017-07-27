3302c17f4de8f97b7fbcf616e8781552bfbe62d4
Edoardo Putti <edoardo.putti@gmail.com>
2017-07-27 11:09:05 +0200

    [airos] added test for language property in gui property

--- tests/airos/test_gui.py
+++ tests/airos/test_gui.py
@@ -20,3 +20,21 @@ class TestGuiConverter(ConverterTest):
         ]
 
         self.assertEqualConfig(o.intermediate_data['gui'], expected)
+
+    def test_language(self):
+        o = self.backend({
+            'gui': {
+                'language' : 'it_IT',
+            },
+        })
+        o.to_intermediate()
+        expected = [
+            {
+                'language': 'it_IT',
+            },
+            {
+                'network.advanced.status': 'enabled',
+            },
+        ]
+
+        self.assertEqualConfig(o.intermediate_data['gui'], expected)
