7c07e9db3ea5a1db49173a17cd13b0dea3fc8615
Ritwick DSouza <ritwick.dsouza@outlook.com>
2017-08-07 17:03:15 +0530

    [raspbian] Added tests for dhcp interface static routes

--- tests/raspbian/test_routes.py
+++ tests/raspbian/test_routes.py
@@ -18,8 +18,7 @@ class TestStaticRoute(unittest.TestCase, _TabsMixin):
                 {
                     "device": "eth0",
                     "destination": "192.168.4.1/24",
-                    "next": "192.168.2.2",
-                    "cost": 2,
+                    "next": "192.168.2.2"
                 },
             ]
         })
@@ -54,8 +53,7 @@ pre-up route del -net 192.168.4.1 netmask 255.255.255.0 gw 192.168.2.2
                 {
                     "device": "eth0",
                     "destination": "192.168.4.1/24",
-                    "next": "192.168.2.2",
-                    "cost": 2,
+                    "next": "192.168.2.2"
                 },
             ]
         })
@@ -66,6 +64,39 @@ auto eth0
 iface eth0 inet static
 address 10.0.0.1
 netmask 255.255.255.240
+post-up route add -net 192.168.4.1 netmask 255.255.255.0 gw 192.168.2.2
+pre-up route del -net 192.168.4.1 netmask 255.255.255.0 gw 192.168.2.2
+
+'''
+        self.assertEqual(o.render(), expected)
+
+    def test_ipv4_dchp_route(self):
+        o = Raspbian({
+            "interfaces": [
+                {
+                    "name": "eth0",
+                    "type": "ethernet",
+                    "addresses": [
+                        {
+                            "family": "ipv4",
+                            "proto": "dhcp"
+                        }
+                    ]
+                }
+            ],
+            "routes": [
+                {
+                    "device": "eth0",
+                    "destination": "192.168.4.1/24",
+                    "next": "192.168.2.2"
+                },
+            ]
+        })
+
+        expected = '''# config: /etc/network/interfaces
+
+auto eth0
+iface eth0 inet dhcp
 post-up route add -net 192.168.4.1 netmask 255.255.255.0 gw 192.168.2.2
 pre-up route del -net 192.168.4.1 netmask 255.255.255.0 gw 192.168.2.2
 
@@ -133,6 +164,39 @@ auto eth0
 iface eth0 inet6 static
 address fe80::ba27:ebff:fe1c:5477
 netmask 64
+up ip -6 route add fd89::1/128 via fd88::1 dev eth0
+down ip -6 route del fd89::1/128 via fd88::1 dev eth0
+
+'''
+        self.assertEqual(o.render(), expected)
+
+    def test_ipv6_dhcp_route(self):
+        o = Raspbian({
+            "interfaces": [
+                {
+                    "name": "eth0",
+                    "type": "ethernet",
+                    "addresses": [
+                        {
+                            "family": "ipv6",
+                            "proto": "dhcp"
+                        }
+                    ]
+                }
+            ],
+            "routes": [
+                {
+                    "device": "eth0",
+                    "destination": "fd89::1/128",
+                    "next": "fd88::1"
+                }
+            ]
+        })
+
+        expected = '''# config: /etc/network/interfaces
+
+auto eth0
+iface eth0 inet6 dhcp
 up ip -6 route add fd89::1/128 via fd88::1 dev eth0
 down ip -6 route del fd89::1/128 via fd88::1 dev eth0
 
