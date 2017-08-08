8fd8a429e3f2f68cc7fd0c67475c33c09f3b62c9
Edoardo Putti <edoardo.putti@gmail.com>
2017-08-08 12:38:15 +0200

    [airos] added test for eap method in wpa2_enterprise for access point

--- tests/airos/test_wpasupplicant.py
+++ tests/airos/test_wpasupplicant.py
@@ -290,3 +290,55 @@ class TestWpasupplicantAccess(ConverterTest):
             }
         ]
         self.assertEqualConfig(o.intermediate_data['wpasupplicant'], expected)
+
+    def test_eap_wpa2_enterprise(self):
+        o = self.backend({
+            "interfaces": [
+                {
+                    "type": "wireless",
+                    "name": "wlan0",
+                    "mac": "de:9f:db:30:c9:c5",
+                    "mtu": 1500,
+                    "txqueuelen": 1000,
+                    "wireless": {
+                        "radio": "radio0",
+                        "mode": "access_point",
+                        "ssid": "ap-ssid-example",
+                        "encryption": {
+                            "protocol": "wpa2_enterprise",
+                            "server": "radius.example.com",
+                            "key": "the-shared-key",
+                            "acct_server": "accounting.example.com",
+                        },
+                    },
+                }
+            ]
+        })
+        o.to_intermediate()
+        expected = [
+            {
+                'device.1.devname': 'radio0',
+                'device.1.driver': 'madwifi',
+                'device.1.profile': 'AUTO',
+                'device.1.status': 'enabled',
+                'profile.1.name': 'AUTO',
+                'profile.1.network.1.anonymous_identity': 'TODO',
+                'profile.1.network.1.eap.1.name': 'TTLS',
+                'profile.1.network.1.eap.1.status': 'enabled',
+                'profile.1.network.1.identity': 'TODO',
+                'profile.1.network.1.key_mgmt.1.name': 'WPA-EAP',
+                'profile.1.network.1.pairwise.1.name': 'CCMP',
+                'profile.1.network.1.password': 'TODO',
+                'profile.1.network.1.phase2=auth': 'MSCHAPV2',
+                'profile.1.network.1.priority': 100,
+                'profile.1.network.1.proto.1.name': 'RSN',
+                'profile.1.network.1.ssid': 'ap-ssid-example',
+                'profile.1.network.2.key_mgmt.1.name': 'NONE',
+                'profile.1.network.2.priority': 2,
+                'profile.1.network.2.status': 'disabled',
+            },
+            {
+                'status': 'enabled',
+            },
+        ]
+        self.assertEqualConfig(o.intermediate_data['wpasupplicant'], expected)
