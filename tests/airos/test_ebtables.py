20d657ca52279737955ded46b470418fa7ca1f1c
Edoardo Putti <edoardo.putti@gmail.com>
2017-08-10 17:14:28 +0200

    [airos] begun testing ebtables converter
    ebtables is configured with respect to mode and protocol of the wireless
    interface

--- tests/airos/test_ebtables.py
+++ tests/airos/test_ebtables.py
@@ -4,8 +4,21 @@ from .mock import ConverterTest, EbtablesAirOs
 class EbtablesConverter(ConverterTest):
     backend = EbtablesAirOs
 
-    def test_ebtables(self):
-        o = self.backend({})
+    def test_station_none(self):
+        o = self.backend({
+            'interfaces': [
+                {
+                    'type': 'wireless',
+                    'name': 'wlan0',
+                    'wireless': {
+                        'mode': 'station',
+                        'radio': 'ath0',
+                        'ssid': 'ubnt',
+                        'bssid': '00:11:22:33:44:55',
+                    }
+                }
+            ]
+        })
         o.to_intermediate()
         expected = [
             {
