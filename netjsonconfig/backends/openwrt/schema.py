92e6240701e40121e87cfdfeac81fe19a2fbf6dd
ValerioBob <montagliani.1759418@studenti.uniroma1.it>
2023-04-21 14:36:03 +0200

    [openwrt] Removed required src from firewall rules schema

--- netjsonconfig/backends/openwrt/schema.py
+++ netjsonconfig/backends/openwrt/schema.py
@@ -1691,7 +1691,7 @@ firewall_properties = {
             "type": "object",
             "title": "Rules",
             "additionalProperties": True,
-            "required": ["src","target"],
+            "required": ["target"],
             "properties": firewall_rules_properties,
         },
     },
