ae0dd1679ac08f00ee9e59358589a70855960229
Federico Capoano <f.capoano@openwisp.io>
2023-04-24 16:33:55 -0400

    [change] OpenWrt: hide roaming UCI if roaming disabled
    If WiFI roaming (802.11r) is not enabled, do not generate the
    UCI config options in the wireless configuration.

--- netjsonconfig/backends/openwrt/converters/wireless.py
+++ netjsonconfig/backends/openwrt/converters/wireless.py
@@ -61,6 +61,7 @@ class Wireless(OpenWrtConverter):
         if 'encryption' in wireless:
             encryption = self.__intermediate_encryption(wireless)
             wireless.update(encryption)
+        wireless = self.__intermediate_roaming(wireless)
         # attached networks (openwrt specific)
         # by default the wifi interface is attached
         # to its defining interface
@@ -138,6 +139,20 @@ class Wireless(OpenWrtConverter):
         if cipher and protocol.startswith('wpa') and cipher != 'auto':
             uci['encryption'] += '+{0}'.format(cipher)
         return uci
+
+    roaming_properties = (
+        'ft_over_ds',
+        'ft_psk_generate_local',
+        'nasid',
+        'reassociation_deadline',
+    )
+
+    def __intermediate_roaming(self, wireless):
+        if wireless.get('ieee80211r') is False:
+            for property in self.roaming_properties:
+                if property in wireless:
+                    del wireless[property]
+        return wireless
 
     def to_netjson_loop(self, block, result, index):
         is_new = False
