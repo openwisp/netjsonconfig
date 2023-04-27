b43eebec1cf7e57adfd428a53785986f4d7226a2
ValerioBob <montagliani.1759418@studenti.uniroma1.it>
2023-04-27 13:57:39 +0200

    [openwrt] Fix mwan3 globals schema

--- netjsonconfig/backends/openwrt/schema.py
+++ netjsonconfig/backends/openwrt/schema.py
@@ -1742,51 +1742,50 @@ mwan3 = {
                 "globals": {
                     "type": "object",
                     "title": "Globals",
+                    "description": "Globals for mwan3",
                     "propertyOrder": 1,
-                    "items": {
-                        "required": [
-                            "mmx_mask",
-                        ],
-                        "properties": {
-                            "mmx_mask": {
-                                "type": "string",
-                                "title": "Firewall mask",
-                                "description": "Enter value in hex, starting with 0x",
-                                "propertyOrder": 1,
-                                "default": "0x3F00",
-                            },
-                            "logging": {
-                                "type": "boolean",
-                                "title": "Logging",
-                                "description": "Global firewall logging. "
-                                "This must be enabled for any rule specific logging to occur",
-                                "propertyOrder": 2,
-                                "default": False,
-                            },
-                            "loglevel": {
-                                "type": "string",
-                                "title": "Log Level",
-                                "description": "Firewall loglevel",
-                                "enum": [
-                                    "emerg",
-                                    "alert",
-                                    "crit",
-                                    "error",
-                                    "warning",
-                                    "notice",
-                                    "info",
-                                    "debug",
-                                ],
-                                "default": "notice",
-                                "propertyOrder": 3,
-                            },
-                            "rt_table_lookup": {
-                                "type": "number",
-                                "title": "Routing table lookup",
-                                "description": "Specify an additional routing table to be scanned  "
-                                "for connected networks. In default config leave empty",
-                                "propertyOrder": 4,
-                            },
+                    "required": [
+                        "mmx_mask",
+                    ],
+                    "properties": {
+                        "mmx_mask": {
+                            "type": "string",
+                            "title": "Firewall mask",
+                            "description": "Enter value in hex, starting with 0x",
+                            "propertyOrder": 1,
+                            "default": "0x3F00",
+                        },
+                        "logging": {
+                            "type": "boolean",
+                            "title": "Logging",
+                            "description": "Global firewall logging. "
+                            "This must be enabled for any rule specific logging to occur",
+                            "propertyOrder": 2,
+                            "default": False,
+                        },
+                        "loglevel": {
+                            "type": "string",
+                            "title": "Log Level",
+                            "description": "Firewall loglevel",
+                            "enum": [
+                                "emerg",
+                                "alert",
+                                "crit",
+                                "error",
+                                "warning",
+                                "notice",
+                                "info",
+                                "debug",
+                            ],
+                            "default": "notice",
+                            "propertyOrder": 3,
+                        },
+                        "rt_table_lookup": {
+                            "type": "number",
+                            "title": "Routing table lookup",
+                            "description": "Specify an additional routing table to be scanned  "
+                            "for connected networks. In default config leave empty",
+                            "propertyOrder": 4,
                         },
                     },
                 },
