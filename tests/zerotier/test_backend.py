3bf46e19a74981b4c1242185fd7e0bca7bc8484f
Aryamanz29 <aryamanz29@gmail.com>
2023-05-31 20:15:48 +0530

    [changes] Extended ZeroTier backend schema
    The schema is taken from OpenAPI specification:
    
    https://docs.zerotier.com/service/v1/ (self-hosted controllers)
    https://docs.zerotier.com/openapi/centralv1.json (central controllers)
    
    The Zerotier network configuration keys taken from:
    
    https://github.com/zerotier/ZeroTierOne/blob/dev/node/NetworkConfig.hpp

--- tests/zerotier/test_backend.py
+++ tests/zerotier/test_backend.py
@@ -14,15 +14,50 @@ class TestBackend(unittest.TestCase):
     _TEST_CONFIG = {
         "zerotier": [
             {
-                "id": "79vgjhks7ae448c5",
-                "name": "network-network-1",
+                "id": "9536600adf0af076",
+                "nwid": "9536600adf0af076",
+                "objtype": "network",
+                "revision": 1,
+                "creationTime": 1632012345,
+                "name": "zerotier-openwisp-network",
                 "private": True,
-            },
-            {
-                "id": "yt6c2e21c0fhhtyu",
-                "name": "zerotier-network-2",
-                "private": False,
-            },
+                "enableBroadcast": True,
+                "v4AssignMode": {"zt": True},
+                "v6AssignMode": {"6plane": False, "rfc4193": True, "zt": True},
+                "mtu": 2700,
+                "multicastLimit": 16,
+                "routes": [{"target": "10.0.0.0/24", "via": "10.0.0.1"}],
+                "ipAssignmentPools": [
+                    {"ipRangeStart": "10.0.0.10", "ipRangeEnd": "10.0.0.100"}
+                ],
+                "dns": {"domain": "zerotier.openwisp.io", "servers": ["10.147.20.3"]},
+                "rules": [
+                    {
+                        "etherType": 2048,
+                        "not": True,
+                        "or": False,
+                        "type": "MATCH_ETHERTYPE",
+                    },
+                    {"type": "ACTION_DROP"},
+                ],
+                "capabilities": [
+                    {
+                        "default": True,
+                        "id": 1,
+                        "rules": [
+                            {
+                                "etherType": 2048,
+                                "not": True,
+                                "or": False,
+                                "type": "MATCH_ETHERTYPE",
+                            }
+                        ],
+                    }
+                ],
+                "tags": [{"default": 1, "id": 1}],
+                "remoteTraceTarget": "7f5d90eb87",
+                "remoteTraceLevel": 1,
+            }
         ]
     }
 
@@ -35,39 +70,72 @@ class TestBackend(unittest.TestCase):
 
     def test_confs(self):
         c = ZeroTier(self._TEST_CONFIG)
-        expected = """# zerotier config: 79vgjhks7ae448c5
+        expected = """# zerotier config: 9536600adf0af076
 
-n=network-network-1
-nwid=79vgjhks7ae448c5
-private=True
-
-# zerotier config: yt6c2e21c0fhhtyu
-
-n=zerotier-network-2
-nwid=yt6c2e21c0fhhtyu
-private=False
-"""
+CAP={'default': True, 'id': 1, 'rules': [{'etherType': 2048, 'not': True, 'or': False, 'type': 'MATCH_ETHERTYPE'}]}
+DNS=domain
+DNS=servers
+I={'ipRangeStart': '10.0.0.10', 'ipRangeEnd': '10.0.0.100'}
+R={'etherType': 2048, 'not': True, 'or': False, 'type': 'MATCH_ETHERTYPE'}
+R={'type': 'ACTION_DROP'}
+RT={'target': '10.0.0.0/24', 'via': '10.0.0.1'}
+TAG={'default': 1, 'id': 1}
+eb=True
+ml=16
+mtu=2700
+n=zerotier-openwisp-network
+nwid=9536600adf0af076
+p=True
+r=1
+t=network
+tl=1
+ts=1632012345
+tt=7f5d90eb87
+v4s=zt
+v6s=6plane
+v6s=rfc4193
+v6s=zt
+"""  # noqa
         self.assertEqual(c.render(), expected)
 
     def test_generate(self):
         c = ZeroTier(self._TEST_CONFIG)
+        expected = """CAP={'default': True, 'id': 1, 'rules': [{'etherType': 2048, 'not': True, 'or': False, 'type': 'MATCH_ETHERTYPE'}]}
+DNS=domain
+DNS=servers
+I={'ipRangeStart': '10.0.0.10', 'ipRangeEnd': '10.0.0.100'}
+R={'etherType': 2048, 'not': True, 'or': False, 'type': 'MATCH_ETHERTYPE'}
+R={'type': 'ACTION_DROP'}
+RT={'target': '10.0.0.0/24', 'via': '10.0.0.1'}
+TAG={'default': 1, 'id': 1}
+eb=True
+ml=16
+mtu=2700
+n=zerotier-openwisp-network
+nwid=9536600adf0af076
+p=True
+r=1
+t=network
+tl=1
+ts=1632012345
+tt=7f5d90eb87
+v4s=zt
+v6s=6plane
+v6s=rfc4193
+v6s=zt
+"""  # noqa
         tar = tarfile.open(fileobj=c.generate(), mode="r")
-        # tar object should contain both zerotier configuration
-        self.assertEqual(len(tar.getmembers()), 2)
-        vpn1 = tar.getmember("79vgjhks7ae448c5.conf")
-        contents = tar.extractfile(vpn1).read().decode()
-        expected = """n=network-network-1
-nwid=79vgjhks7ae448c5
-private=True
-"""
+        self.assertEqual(len(tar.getmembers()), 1)
+        vpn = tar.getmember("9536600adf0af076.conf")
+        contents = tar.extractfile(vpn).read().decode()
         self.assertEqual(contents, expected)
 
     def test_auto_client(self):
         expected = {
             "zerotier": [
                 {
-                    "id": "79vgjhks7ae448c5",
-                    "name": "network-network-1",
+                    "id": "9536600adf0af076",
+                    "name": "zerotier-openwisp-network",
                 }
             ]
         }
