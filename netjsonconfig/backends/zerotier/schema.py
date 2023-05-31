95668c7e10b1cbcc216e2c8997074b2959ab2718
Aryamanz29 <aryamanz29@gmail.com>
2023-05-31 20:15:48 +0530

    [changes] Extended ZeroTier backend schema
    The schema is taken from OpenAPI specification:
    
    https://docs.zerotier.com/service/v1/ (self-hosted controllers)
    https://docs.zerotier.com/openapi/centralv1.json (central controllers)
    
    The Zerotier network configuration keys taken from:
    
    https://github.com/zerotier/ZeroTierOne/blob/dev/node/NetworkConfig.hpp

--- netjsonconfig/backends/zerotier/schema.py
+++ netjsonconfig/backends/zerotier/schema.py
@@ -24,42 +24,175 @@ base_zerotier_schema = {
                 "type": "object",
                 "title": "ZeroTier",
                 "additionalProperties": True,
-                "required": ["name", "id"],
+                "required": ["name"],
                 "properties": {
+                    # Read-only properties
                     "id": {
-                        "title": "network id",
-                        "description": "ZeroTier network ID",
                         "type": "string",
-                        "minLength": 16,
-                        "maxLength": 16,
-                        "propertyOrder": 1,
+                        "readOnly": True,
+                        "example": "3e245e31af000001",
+                        "description": "Network ID",
                     },
                     "nwid": {
-                        "title": "network id (legacy field)",
-                        "description": "ZeroTier network ID (legacy field)",
                         "type": "string",
-                        "minLength": 16,
-                        "maxLength": 16,
-                        "propertyOrder": 2,
+                        "readOnly": True,
+                        "example": "3e245e31af000001",
+                        "description": "Network ID legacy field (same as 'id')",
                     },
-                    "name": {
-                        "title": "network name",
-                        "description": "ZeroTier network name",
+                    "objtype": {
                         "type": "string",
-                        "propertyOrder": 3,
+                        "readOnly": True,
+                        "example": "network",
+                    },
+                    "revision": {
+                        "type": "integer",
+                        "example": 1,
+                        "readOnly": True,
+                        "description": "The revision number of the network configuration",
+                    },
+                    "creationTime": {
+                        "type": "number",
+                        "readOnly": True,
+                        "example": 1623101592,
+                        "description": "Time the network was created",
+                    },
+                    # Configurable properties
+                    "name": {
+                        "type": "string",
+                        "example": "openwisp-wifi-network",
+                        "description": "Name of the network",
                     },
                     "private": {
-                        "title": "private",
                         "type": "boolean",
                         "description": (
-                            "Whether or not the zerotier network is private."
-                            "If false, members will NOT need to be authorized to join.",
+                            "Whether or not the network is private."
+                            "If false, members will *NOT* need to be authorized to join."
                         ),
-                        "propertyOrder": 4,
+                    },
+                    "enableBroadcast": {
+                        "type": "boolean",
+                        "description": "Enable broadcast packets on the network",
+                    },
+                    "v4AssignMode": {
+                        "type": "object",
+                        "properties": {
+                            "zt": {"type": "boolean"},
+                            "description": "Whether ZeroTier should assign IPv4 addresses to members",
+                        },
+                    },
+                    "v6AssignMode": {
+                        "type": "object",
+                        "properties": {
+                            "6plane": {
+                                "type": "boolean",
+                                "description": "Whether 6PLANE addressing should be used for IPv6 assignment",
+                            },
+                            "rfc4193": {
+                                "type": "boolean",
+                                "description": "Whether RFC4193 addressing should be used for IPv6 assignment",  # noqa
+                            },
+                            "zt": {
+                                "type": "boolean",
+                                "description": "Whether ZeroTier should assign IPv6 addresses to members",
+                            },
+                        },
+                    },
+                    "mtu": {
+                        "type": "integer",
+                        "example": 2800,
+                        "description": "MTU to set on the client virtual network adapter",
+                    },
+                    "multicastLimit": {
+                        "type": "integer",
+                        "example": 32,
+                        "description": (
+                            "Maximum number of recipients per multicast or broadcast."
+                            "Warning - Setting this to 0 will disable IPv4 communication on your network!"
+                        ),
+                    },
+                    "routes": {
+                        "type": "array",
+                        "items": {
+                            "type": "object",
+                            "properties": {
+                                "target": {
+                                    "type": "string",
+                                    "example": "192.168.192.0/24",
+                                    "description": "The target IP address range for the route",
+                                },
+                                "via": {
+                                    "type": "string",
+                                    "example": "192.168.192.1",
+                                    "description": "The IP address of the next hop for the route",
+                                },
+                            },
+                        },
+                        "description": "Array of route objects",
+                    },
+                    "ipAssignmentPools": {
+                        "type": "array",
+                        "items": {
+                            "type": "object",
+                            "properties": {
+                                "ipRangeStart": {
+                                    "type": "string",
+                                    "example": "192.168.192.1",
+                                    "description": "The starting IP address of the pool range",
+                                },
+                                "ipRangeEnd": {
+                                    "type": "string",
+                                    "example": "192.168.192.254",
+                                    "description": "The ending IP address of the pool range",
+                                },
+                            },
+                        },
+                        "description": "Range of IP addresses for the auto assign pool",
+                    },
+                    "dns": {
+                        "type": "object",
+                        "properties": {
+                            "domain": {
+                                "type": "string",
+                                "example": "zerotier.openwisp.io",
+                                "description": "The domain for DNS resolution",
+                            },
+                            "servers": {
+                                "type": "array",
+                                "items": {
+                                    "type": "string",
+                                    "example": "10.147.20.3",
+                                    "description": "The DNS server IP addresses",
+                                },
+                            },
+                        },
+                    },
+                    "rules": {
+                        "type": "array",
+                        "items": {"type": "object"},
+                        "description": "Array of network rule objects",
+                    },
+                    "capabilities": {
+                        "type": "array",
+                        "items": {"type": "object"},
+                        "description": "Array of network capabilities",
+                    },
+                    "tags": {
+                        "type": "array",
+                        "items": {"type": "object"},
+                        "description": "Array of network tag objects",
+                    },
+                    "remoteTraceTarget": {
+                        "type": "string",
+                        "example": "7f5d90eb87",
+                        "description": "The remote target ID for network tracing",
+                    },
+                    "remoteTraceLevel": {
+                        "type": "integer",
+                        "description": "The level of network tracing",
                     },
                 },
             },
-        }
+        },
     },
 }
 
